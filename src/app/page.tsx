e851905fcf3d1a5f26f0c7a56c88144bd945bb9c
Eadweard <804875728@qq.com>
2023-03-17 15:05:37 +0800

    一些工程化配置

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,6 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '../styles/page.module.css'
 
-const inter = Inter({ subsets: ['latin'] })
-
 export default function Home() {
   return (
     <main className={styles.main}>
