b291e6b271ad66daca0f54884f9dc86169830f0e
Eadweard <804875728@qq.com>
2023-04-04 10:04:21 +0800

    useFrame

--- src/ui/Hero.tsx
+++ src/ui/Hero.tsx
@@ -1,13 +1,22 @@
 "use client"
 
-import { FC, Suspense } from 'react'
+import { FC, Suspense, useRef } from 'react'
 import styles from '~/styles/common.module.scss'
 import '~/styles/hero.scss'
 import NavBar from '~/components/navbar'
-import { Canvas } from "@react-three/fiber";
+import { Canvas, useFrame } from "@react-three/fiber";
 import { OrbitControls, Sphere, MeshDistortMaterial } from "@react-three/drei";
 
 const Hero: FC = () => {
+  const sphereRef = useRef<Sphere>();
+  function ThreeFrame() {
+    useFrame(({ clock }) => {
+      sphereRef.current.rotation.x += 0.02;
+      sphereRef.current.rotation.y += 0.02;
+    })
+    return null
+  }
+
   return (
     <div className={styles.section}>
       <NavBar />
@@ -23,7 +32,7 @@ const Hero: FC = () => {
               <ambientLight intensity={1} />
               {/* eslint-disable-next-line react/no-unknown-property */}
               <directionalLight position={[3, 2, 1]} />
-              <Sphere args={[1, 100, 200]} scale={2.4}>
+              <Sphere ref={sphereRef} args={[1, 100, 200]} scale={2.4}>
                 <MeshDistortMaterial
                   color="#3d1c56"
                   attach="material"
@@ -31,6 +40,7 @@ const Hero: FC = () => {
                   speed={2}
                 />
               </Sphere>
+              <ThreeFrame />
             </Suspense>
           </Canvas>
           <img className='three_img' src="/img/moon.png" alt="/img/moon.png" />
