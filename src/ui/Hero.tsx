8cd1da0a9adfa8e95dc9d45d3ab5be2b8e7b2b4a
Eadweard <804875728@qq.com>
2023-05-10 18:35:38 +0800

    优化

--- src/ui/Hero.tsx
+++ src/ui/Hero.tsx
@@ -1,12 +1,11 @@
 "use client"
-
 import { FC, Suspense, useRef } from 'react'
 import styles from '~/styles/common.module.scss'
 import heroStyles from '~/styles/hero.module.scss'
 import { Canvas, useFrame } from "@react-three/fiber";
 import { OrbitControls, Sphere, MeshDistortMaterial } from "@react-three/drei";
 
-const Hero: FC = () => {
+const ThreeRender = () => {
   const sphereRef = useRef<any>();
   function ThreeFrame() {
     useFrame(({ clock }) => {
@@ -15,32 +14,38 @@ const Hero: FC = () => {
     })
     return null
   }
+  return (
+    <Canvas>
+      <Suspense fallback={null}>
+        <OrbitControls enableZoom={false} />
+        {/* eslint-disable-next-line react/no-unknown-property */}
+        <ambientLight intensity={1} />
+        {/* eslint-disable-next-line react/no-unknown-property */}
+        <directionalLight position={[3, 2, 1]} />
+        <Sphere ref={sphereRef} args={[1, 100, 200]} scale={2.4}>
+          <MeshDistortMaterial
+            color="#394E6A"
+            attach="material"
+            distort={0.3}
+            speed={2}
+          />
+        </Sphere>
+        <ThreeFrame />
+      </Suspense>
+    </Canvas>
+  )
+}
 
+const Hero: FC = () => {
   return (
     <div className={styles.section}>
       <div className={styles.container}>
         <div className={heroStyles.left}>
           <span className={heroStyles.title}>Introduction</span>
+          <p>一个前端开发者</p>
         </div>
         <div className={heroStyles.right}>
-          <Canvas>
-            <Suspense fallback={null}>
-              <OrbitControls enableZoom={false} />
-              {/* eslint-disable-next-line react/no-unknown-property */}
-              <ambientLight intensity={1} />
-              {/* eslint-disable-next-line react/no-unknown-property */}
-              <directionalLight position={[3, 2, 1]} />
-              <Sphere ref={sphereRef} args={[1, 100, 200]} scale={2.4}>
-                <MeshDistortMaterial
-                  color="#394E6A"
-                  attach="material"
-                  distort={0.3}
-                  speed={2}
-                />
-              </Sphere>
-              <ThreeFrame />
-            </Suspense>
-          </Canvas>
+          <ThreeRender />
           <img className={heroStyles.three_img} src="/img/moon.png" alt="/img/moon.png" />
         </div>
       </div>
