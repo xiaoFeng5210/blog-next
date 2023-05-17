7a60be19f91fc960b2ae5597ad5fd6fda82c4b2a
Eadweard <804875728@qq.com>
2023-05-17 11:11:26 +0800

    navbar finished

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,30 +1,29 @@
 "use client"
-import { FC, useEffect, useRef } from 'react'
+import { FC, useEffect, useMemo, useRef, useState } from 'react'
 import styles from './navbar.module.scss'
 
+type HandleScroll = (e: boolean) => void
 const getNames = () => {
   return ['主页', '动效作品', '2d作品', '3d作品', '个人介绍']
 }
 
-const useScroll = () => {
+
+const useScroll = (handle: HandleScroll) => {
   const backdrop = useRef(null)
   const handleScroll = (e: Event) => {
-    console.log('滚动')
     // 获取当前滚动的垂直位置
     const scrollTop = document.documentElement.scrollTop;
-    console.log(e)
+    console.log(scrollTop)
     // 检查滚动位置，根据需要进行样式变化
     if (scrollTop > 0) {
       // 滚动时显示背景效果
-      console.log(backdrop)
-
+      handle(true)
     } else {
       // 滚动到顶部时恢复初始样式
-
+      handle(false)
     }
   }
   useEffect(() => {
-    console.log('初始化')
     window.addEventListener('scroll', handleScroll)
     return () => {
       window.removeEventListener('scroll', handleScroll)
@@ -34,9 +33,16 @@ const useScroll = () => {
 }
 
 const NavBar: FC = () => {
-  const { backdrop } = useScroll()
+  const [isShowBackdrop, setIsShowBackdrop] = useState(false)
+  const { backdrop } = useScroll(setIsShowBackdrop)
+  const isShowBackdropStyles = useMemo(() => {
+    return isShowBackdrop ? {
+      backdropFilter: 'blur(5px)',
+      background: 'hsla(0, 0%, 100%, .75)'
+    } : {}
+  }, [isShowBackdrop])
   return (
-    <div ref={backdrop} className={styles.section}>
+    <div ref={backdrop} className={styles.section} style={isShowBackdropStyles}>
       <div className={styles.container}>
         <div className={styles.links}>
           <span className={styles.logo}>Aurora</span>
