8076997b7db3152628983167ae97d5621f96f693
Eadweard <804875728@qq.com>
2023-06-08 17:08:46 +0800

    bar x y轴

--- src/components/d3-chart/bar.tsx
+++ src/components/d3-chart/bar.tsx
@@ -1,18 +1,21 @@
 "use client"
-import d3 from 'd3'
-import { FC } from 'react'
+import * as d3 from 'd3'
+import { FC, useEffect } from 'react'
 import { d3BarList } from '../../local/baseData'
 interface Props {
   d3viewBox: { width: number, height: number }
   index: number
 }
-const component: FC<Props> = (props) => {
+const Bar: FC<Props> = (props) => {
   const { d3viewBox, index } = props
+  useEffect(() => {
+    drawBars()
+  }, [d3viewBox, index])
   const drawBars = () => {
     const { width, height } = d3viewBox
     const svg = d3.select(`#d3_show_chart${index}`)
       .append('svg').attr('width', width).attr('height', height)
-    const padding = { top: 20, right: 30, bottom: 20, left: 30 }
+    const padding = { top: 20, right: 50, bottom: 200, left: 50 }
     const dataset = d3BarList.map(item => item.value as number)
     const xScale = d3.scaleBand().domain(d3BarList.map(item => item.name)).range([0, width - padding.left - padding.right])
     const yScale = d3.scaleLinear().domain([0, d3.max(dataset) as number])
@@ -30,4 +33,4 @@ const component: FC<Props> = (props) => {
     <div id={`d3_show_chart${index}`} className="d3_show_chart w-[100%] h-[100%]" />
   )
 }
-export default component
+export default Bar
--- src/ui/Works.tsx
+++ src/ui/Works.tsx
@@ -2,8 +2,10 @@
 import { FC, useEffect, useState } from 'react'
 import styles from '../styles/common.module.scss'
 import '../styles/works.scss'
-import * as d3 from 'd3'
 import { bubbleList, d3Tabs } from '../local/baseData'
+import Bar from '../components/d3-chart/bar'
+
+const d3ChartComponents = [Bar]
 
 const Works: FC = () => {
   const [selectKey, setSelectKey] = useState(0)
@@ -20,18 +22,7 @@ const Works: FC = () => {
     const height = document.getElementById('d3_show_box')?.offsetHeight
     return { width, height }
   }
-  // const drawBubble = () => {
-  //   const COLOR = ['#4B0082', '#9932CC', '#9400D3', '#BA55D3']
-  //   const values = bubbleList.map(item => item.value as number)
-  //   const max = Math.max(...values)
-  //   const min = Math.min(...values)
-  //   const scale = d3.scaleLinear([min, max], [16, 40])
-  //   const scaleFontSize = d3.scaleLinear([min, max], [20, 38])
-  //   const scaleCategory = d3.scaleLinear([min, max], [0, COLOR.length - 1])
-  //   const color = (value: number) => {
-  //     return COLOR[Math.floor(scaleCategory(value))]
-  //   }
-  // }
+
   return (
     <div className={styles.section}>
       <div className='works_box'>
@@ -46,15 +37,37 @@ const Works: FC = () => {
               }
             </div>
           </div>
-
           {/* 显示内容 */}
           <div id='d3_show_box' className="d3_content w-[100%] h-[100%]">
-
+            {switchComponent(selectKey, d3viewBox)}
           </div>
         </div>
       </div>
     </div>
   )
 }
+
+const switchComponent = (index: number, d3viewBox: { width: number, height: number }) => {
+  const Component = d3ChartComponents[index]
+  return Component ? <Component d3viewBox={d3viewBox} index={index} /> : <></>
+}
+
 export default Works
+
+
+
+
+
+// const drawBubble = () => {
+  //   const COLOR = ['#4B0082', '#9932CC', '#9400D3', '#BA55D3']
+  //   const values = bubbleList.map(item => item.value as number)
+  //   const max = Math.max(...values)
+  //   const min = Math.min(...values)
+  //   const scale = d3.scaleLinear([min, max], [16, 40])
+  //   const scaleFontSize = d3.scaleLinear([min, max], [20, 38])
+  //   const scaleCategory = d3.scaleLinear([min, max], [0, COLOR.length - 1])
+  //   const color = (value: number) => {
+  //     return COLOR[Math.floor(scaleCategory(value))]
+  //   }
+  // }
 
