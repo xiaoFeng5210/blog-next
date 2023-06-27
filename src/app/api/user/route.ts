be371023e772f31d9e496a68462742f5dc840c45
Eadweard <804875728@qq.com>
2023-06-27 15:27:00 +0800

    增加dockerfile

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -3,9 +3,9 @@ import { NextResponse } from 'next/server';
 import prisma from '../../../../prisma'
 
 // 查询users接口
-export async function GET(req: NextApiRequest, res: NextApiResponse) {
-  const users = await prisma.user.findMany().catch(err => (err))
-  return NextResponse.json(users)
-}
+// export async function GET(req: NextApiRequest, res: NextApiResponse) {
+//   const users = await prisma.user.findMany().catch(err => (err))
+//   return NextResponse.json(users)
+// }
 
 
