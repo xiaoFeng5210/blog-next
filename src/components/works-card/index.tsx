6028619c63c7f30dd4704629d7ed65cd778ce7cd
Eadweard <804875728@qq.com>
2023-07-06 17:18:11 +0800

    fix

---
+++ src/components/works-card/index.tsx
@@ -0,0 +1,21 @@
+"use client"
+import type { FC } from 'react'
+import './works-card.scss'
+import Image from 'next/image'
+
+const WorkCard: FC = () => {
+  return (
+    <div className='card_box'>
+      <div className='w-full h-[200px] image_box'>
+        <Image src="/img/works/natrue.jpg" alt='test' fill={true} style={{ objectFit: "cover" }} />
+      </div>
+
+      <div className="content p-[16px] w-full h-full">
+        <h3 className="text-[20px] font-bold text-[#11AFFA]">标题小样式</h3>
+        <p>基于 NestJs + Vben Admin 编写的一款前后端分离的权限管理系统</p>
+      </div>
+
+    </div>
+  )
+}
+export default WorkCard
--- src/ui/WorkList.tsx
+++ src/ui/WorkList.tsx
@@ -1,10 +1,13 @@
 "use client"
 import type { FC } from 'react'
 import '~/styles/workList.scss'
+import WorkCard from '~/components/works-card'
+
 const WorkList: FC = () => {
   return (
     <main className=''>
-      
+      <WorkCard />
+      <WorkCard />
     </main>
   )
 }
