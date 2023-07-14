c1094be416671f6fb7e7250a4e15089c85c4b64f
Eadweard <804875728@qq.com>
2023-07-14 15:19:03 +0800

    fix

--- src/core/watermark/Watermark.tsx
+++ src/core/watermark/Watermark.tsx
@@ -15,7 +15,7 @@ const Watermark: FC = () => {
         const base64 = e.target?.result as string
         if (base64) {
           imgTogether(base64, (str) => {
-            const size = 180 / (window.devicePixelRatio || 1)
+            const size = 500 / (window.devicePixelRatio || 1)
             eleImgUploadX.innerHTML = `<img src="${str}" width="${size}" height="${size}" />`
           })
         } else {
@@ -29,7 +29,7 @@ const Watermark: FC = () => {
   }, [])
   const imgTogether = (url: string, callback: (str: string) => void) => {
     const canvas = document.createElement('canvas')
-    const size = 180;
+    const size = 500;
     canvas.width = size
     canvas.height = size
     const context = canvas.getContext('2d')
@@ -43,9 +43,9 @@ const Watermark: FC = () => {
     imgUpload.src = url
   }
   return (
-    <div className='w-full h-full'>
+    <div className='w-full h-full flex flex-col items-center justify-center'>
       <input type="file" id="uploadFile" className='clip' accept="image/*" />
-      <label className='ui-button ui-button-primary' htmlFor="uploadFile">选择图片</label>
+      {/* <label className='ui-button ui-button-primary' htmlFor="uploadFile">选择图片</label> */}
       <img id="imgCover" src="/img/works/watermark.png" className="clip"></img>
       <p id="imgUploadX"></p>
     </div>
