639e79359a1967ee73f28a8c00f03355eaa737b8
Eadweard <804875728@qq.com>
2023-08-10 13:09:41 +0800

    feat:🚀textfold

---
+++ src/core/text-fold/TextFold.tsx
@@ -0,0 +1,11 @@
+"use client"
+import type { FC } from 'react'
+import CoreContainer from '~/ui/CoreContainer'
+const TextFold: FC = () => {
+  return (
+    <CoreContainer title='多行文本展开收起'>
+      
+    </CoreContainer>
+  )
+}
+export default TextFold
