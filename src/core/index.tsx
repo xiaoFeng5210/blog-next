b8eac38153486becb36fd403c46b4959534e1d87
Eadweard <804875728@qq.com>
2023-08-10 13:34:29 +0800

    feat: 🚀性能提升

--- src/core/index.tsx
+++ src/core/index.tsx
@@ -2,6 +2,7 @@ import Watermark from "./watermark/Watermark"
 import GradentBorder from "./gradent-border/GradentBorder"
 import { worksSetting } from "~/local/works-setting"
 import { WorksHrefEnum } from "~/types"
+import TextFold from "./text-fold/TextFold"
 
 export const workShow = (id: number) => {
   const workOnlyOne = worksSetting.find((item) => item.id === id)
@@ -12,7 +13,7 @@ export const workShow = (id: number) => {
       case WorksHrefEnum.gradientBorder:
         return <GradentBorder />
       case WorksHrefEnum.textFold:
-        return   
+        return <TextFold />
       default:
         return <div>暂无</div>
     }    
