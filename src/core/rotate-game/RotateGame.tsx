5a75d139153470fee765048eeb18a8c2e6e3829b
Eadweard <804875728@qq.com>
2023-08-11 14:09:07 +0800

    fix prize-item-wrapper

--- src/core/rotate-game/RotateGame.tsx
+++ src/core/rotate-game/RotateGame.tsx
@@ -1,8 +1,13 @@
-import type { FC } from 'react'
+import { FC, useMemo } from 'react'
 import CoreContainer from '~/ui/CoreContainer'
+import { prizeListOrigin } from './constants'
 import './rotate-game.scss'
 
 const RotateGame: FC = () => {
+  const prizeNumber = prizeListOrigin.length
+  const prizeList = useMemo(() => {
+    return prizeListOrigin.slice(0, prizeNumber)
+  }, [prizeNumber])
   return (
     <CoreContainer title='css转盘抽奖'>
       <div className='flex justify-center'>
@@ -10,7 +15,17 @@ const RotateGame: FC = () => {
           <div className="wheel-pointer">Start</div>
           <div className="wheel-bg">
             <div className="prize-list">
-              
+              {
+                prizeList.map((item, index) => {
+                  return (
+                    <div key={index} className="prize-item-wrapper">
+                      <div className="prize-item">
+                        
+                      </div>
+                    </div>
+                  )
+                })
+              }
             </div>
           </div>
         </div>
