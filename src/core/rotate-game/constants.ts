1c5dc1d6af9260f6ed5d26d02f0d49ff508f3264
Eadweard <804875728@qq.com>
2023-08-11 14:09:02 +0800

    prize-item-wrapper

---
+++ src/core/rotate-game/constants.ts
@@ -0,0 +1,34 @@
+export const prizeListOrigin = [
+  {
+    icon: "https://picsum.photos/40?random=1",
+    name: "$10000",
+  },
+  {
+    icon: "https://picsum.photos/40?random=6",
+    name: "Thank you!",
+  },
+  {
+    icon: "https://picsum.photos/40?random=2",
+    name: "$500",
+  },
+  {
+    icon: "https://picsum.photos/40?random=3",
+    name: "$100",
+  },
+  {
+    icon: "https://picsum.photos/40?random=6",
+    name: "Thank you!",
+  },
+  {
+    icon: "https://picsum.photos/40?random=4",
+    name: "$50",
+  },
+  {
+    icon: "https://picsum.photos/40?random=5",
+    name: "$10",
+  },
+  {
+    icon: "https://picsum.photos/40?random=6",
+    name: "Thank you!",
+  },
+]
