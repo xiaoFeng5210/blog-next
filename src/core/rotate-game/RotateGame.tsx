3b7e87414a0d7df13f14e34d9812d9b207a15b68
Eadweard <804875728@qq.com>
2023-08-17 10:58:54 +0800

    滚轮完成

--- src/core/rotate-game/RotateGame.tsx
+++ src/core/rotate-game/RotateGame.tsx
@@ -1,19 +1,41 @@
-import { FC, useMemo } from 'react'
+"use client"
+
+import { FC, useEffect, useMemo, useState } from 'react'
 import CoreContainer from '~/ui/CoreContainer'
 import { prizeListOrigin } from './constants'
 import './rotate-game.scss'
 
 const RotateGame: FC = () => {
+  const [rolling, setRolling] = useState(false)
+  const [wheelDeg, setWheelDeg] = useState(0)
+  useEffect(() => {
+    console.log(wheelDeg)
+  }, [wheelDeg])
+
   const prizeNumber = prizeListOrigin.length
   const prizeList = useMemo(() => {
     return prizeListOrigin.slice(0, prizeNumber)
   }, [prizeNumber])
+  const roll = (result: number) => {
+    setRolling(true)
+    setWheelDeg(wheelDeg => (wheelDeg - (wheelDeg % 360) + 360 * 6 +
+      (360 - (360 / prizeList.length) * result)))
+    setTimeout(() => {
+      setRolling(false)
+      alert(`恭喜你获得${prizeList[result].name}`)
+    }, 4500)
+  }
+  const onClickRotate = () => {
+    if (rolling) return
+    const result = Math.floor(Math.random() * prizeList.length)
+    roll(result)
+  }
   return (
     <CoreContainer title='css转盘抽奖'>
       <div className='flex justify-center'>
         <div className="wheel-wrapper">
-          <div className="wheel-pointer">Start</div>
-          <div className="wheel-bg">
+          <div className="wheel-pointer" onClick={onClickRotate}>Start</div>
+          <div className="wheel-bg" style={{transform: `rotate(${wheelDeg}deg)`}}>
             <div className="prize-list">
               {
                 prizeList.map((item, index) => {
